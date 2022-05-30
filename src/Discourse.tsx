e120484c462d02d1b975901ac24ce56fdeaed414
nmorse <morse.nate@gmail.com>
2022-05-30 11:04:48 -0400

    start the four forms

--- src/Discourse.tsx
+++ src/Discourse.tsx
@@ -11,6 +11,57 @@ export const Chat = () => {
         </p>
         <PounceCat pounceCode="5 type-of" ></PounceCat>
       </div>
+      <div className="Feature-child double-wide">
+        <h3>The Four Forms of Judgment</h3>
+        <p>
+          <ol>
+            <li>____ is a ____.
+              <PounceCat pounceCode={`
+5 Nat is-a
+'5' Str is-a
+Nat Type is-a
+
+[[type-of] dip ==] 
+[is-a] compose`} ></PounceCat>
+            </li>
+            <li>____ is the same ____ as ____.
+              <PounceCat pounceCode={`
+5 Nat 5 is-the-same-type-as
+'5' Str '5' is-the-same-type-as
+'a' Str 'B' is-the-same-type-as
+Nat Type Nat is-the-same-type-as
+
+
+[[type-of] dip2 type-of [dup] dip [==] dip2 == &&] 
+[is-the-same-type-as] compose`} ></PounceCat>
+            </li>
+            <li>____ is a type.
+              <PounceCat pounceCode={`
+Nat is-a-type
+5 is-a-type
+'a' is-a-type
+Type is-a-type
+
+
+[type-of dup [Type ==] dip ] 
+[is-a-type] compose
+`} ></PounceCat>
+            </li>
+            <li>____ and ____ are the same type.
+            <PounceCat pounceCode={`
+5 Nat are-the-same-type
+'5' '5' are-the-same-type
+'a' 'B' are-the-same-type
+Nat Type Nat are-the-same-type
+
+
+[[type-of] dip2 type-of [dup] dip [==] dip2 == &&] 
+[are-the-same-type] compose`} ></PounceCat>
+            </li>
+          </ol>
+        </p>
+
+      </div>
     </div>
   );
 };
